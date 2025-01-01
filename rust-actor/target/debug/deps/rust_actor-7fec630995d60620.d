2c3c6e237edff69a93dad823f0cb031632d78b53
mck0324 <dch3030@naver.com>
2025-01-01 15:50:00 +0900

    Setting

---
+++ rust-actor/target/debug/deps/rust_actor-7fec630995d60620.d
@@ -0,0 +1,5 @@
+/Users/minchan/Desktop/study/my_actor/rust-actor/target/debug/deps/librust_actor-7fec630995d60620.rmeta: src/main.rs
+
+/Users/minchan/Desktop/study/my_actor/rust-actor/target/debug/deps/rust_actor-7fec630995d60620.d: src/main.rs
+
+src/main.rs:
---
+++ rust-actor/target/debug/deps/rust_actor-bb63c0bd968c7b28.d
@@ -0,0 +1,5 @@
+/Users/minchan/Desktop/study/my_actor/rust-actor/target/debug/deps/librust_actor-bb63c0bd968c7b28.rmeta: src/main.rs
+
+/Users/minchan/Desktop/study/my_actor/rust-actor/target/debug/deps/rust_actor-bb63c0bd968c7b28.d: src/main.rs
+
+src/main.rs:
